b444d6b2275678ac254a5312a1bd07f258d55d07
Danilina Mariya <mariya199828@gmail.com>
2024-04-21 16:52:38 +0700

    Исправляет слайдер hero и добавляет блок tours

--- source/js/hero.js
+++ source/js/hero.js
@@ -1,13 +1,23 @@
 import Swiper from 'swiper';
-import {Navigation, Pagination} from 'swiper/modules';
+import {Pagination} from 'swiper/modules';
 import 'swiper/css';
 
 new Swiper ('.hero__swiper', {
-  modules: [Navigation, Pagination],
+  modules: [Pagination],
+  loop: true,
+  simulateTouch: true,
+  watchSlidesProgress: true,
+  breakpoints: {
+    1440: {
+      simulateTouch: false
+    }
+  },
 
   pagination: {
     el: '.swiper-pagination',
-    clickable: true
+    clickable: true,
+    type: 'bullets',
+    bulletElement: 'button',
   },
 }
 );
---
+++ source/js/tours.js
@@ -0,0 +1,30 @@
+import Swiper from 'swiper';
+import {Navigation} from 'swiper/modules';
+import 'swiper/css';
+
+new Swiper ('.tours__swiper', {
+  modules: [Navigation],
+  loop: false,
+  simulateTouch: true,
+  watchSlidesProgress: true,
+  breakpoints: {
+    320: {
+      slidesPerView: 1,
+    },
+    768: {
+      slidesPerView: 2,
+      spaceBetween: 18,
+    },
+    1440: {
+      slidesPerView: 3,
+      simulateTouch: false,
+      spaceBetween: 40,
+    }
+  },
+
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  }
+}
+);
